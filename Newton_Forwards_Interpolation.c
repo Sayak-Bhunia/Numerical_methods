f30aa173d1e7d8f62e56f3f97933b8e31f19bd34
Sayak Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2023-11-28 00:19:07 +0530

    Create Newton_Forwards_Interpolation.c

---
+++ Newton_Forwards_Interpolation.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+float find_u(float u, int n)
+{
+    float temp = u;
+    for (int i = 1; i < n; i++)
+        temp = temp * (u - i);
+    return temp;
+}
+
+int factorial(int n)
+{
+    int f = 1;
+    for (int i = 2; i <= n; i++)
+    {
+        f *= i;
+    }
+    return f;
+}
+
+int main()
+{
+    int n = 4;
+    float x[] = {45, 50, 55, 60};
+    float y[n][n];
+    y[0][0] = 0.7071;
+    y[1][0] = 0.7660;
+    y[2][0] = 0.8192;
+    y[3][0] = 0.8660;
+
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = 0; j < n - i; j++)
+            y[j][i] = y[j + 1][i - 1] - y[j][i - 1];
+    }
+
+    printf("Forward Difference : \n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%.4f\t", x[i]);
+        for (int j = 0; j < n - i; j++)
+        {
+            printf("%.4f\t", y[i][j]);
+        }
+        printf("\n");
+    }
+    float value = 45;
+    float sum = y[0][0];
+    float u = (value - x[0]) / (x[1] - x[0]);
+    for (int i = 1; i < n; i++)
+    {
+        sum = sum + (find_u(u, i) * y[0][i]) / factorial(i);
+    }
+    printf("Value at %.2f is %.4f\n", value, sum);
+    return 0;
+}
